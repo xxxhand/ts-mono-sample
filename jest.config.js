acae386f2fa497a2e6d6f5490246ad42ab10f1f1
hand_wu <Hand_Wu@compal.com>
2021-05-28 13:45:19 +0800

    Modify configs

--- jest.config.js
+++ jest.config.js
@@ -3,38 +3,38 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
-    preset: 'ts-jest',
-    bail: 1,
-    testTimeout: 60 * 1000,
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: './public/unit-test/coverage',
-    coveragePathIgnorePatterns: [
-        '/node_modules/'
-    ],
-    coverageProvider: 'v8',
-    moduleFileExtensions: [
-        'js',
-        'jsx',
-        'ts',
-        'tsx'
-    ],
-    testEnvironment: 'node',
-    transform: {
-        ...tsjPreset.transform,
-    },
-    reporters: [
-        'default',
-        [
-            './node_modules/jest-html-reporters',
-            {
-                pageTitle: 'Test report',
-                publicPath: './public/unit-test',
-                filename: 'index.html',
-                expand: false,
-            }
-        ]
-    ],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages' }),
+	preset: 'ts-jest',
+	bail: 1,
+	testTimeout: 60 * 1000,
+	clearMocks: true,
+	collectCoverage: true,
+	coverageDirectory: './public/unit-test/coverage',
+	coveragePathIgnorePatterns: [
+		'/node_modules/'
+	],
+	coverageProvider: 'v8',
+	moduleFileExtensions: [
+		'js',
+		'jsx',
+		'ts',
+		'tsx'
+	],
+	testEnvironment: 'node',
+	transform: {
+		...tsjPreset.transform,
+	},
+	reporters: [
+		'default',
+		[
+			'./node_modules/jest-html-reporters',
+			{
+				pageTitle: 'Test report',
+				publicPath: './public/unit-test',
+				filename: 'index.html',
+				expand: false,
+			}
+		]
+	],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages' }),
 };
 
